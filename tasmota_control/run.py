e309e799be833efcbc7889450debd8b33eabac30
Adam Eagles <ad.c.eagles@gmail.com>
2023-11-14 20:06:52 +0000

    convert back to base redis

--- tasmota_control/run.py
+++ tasmota_control/run.py
@@ -8,7 +8,7 @@ redis_host = os.environ.get("REDIS_HOST", "localhost")
 redis_port = 6379
 redis_db = 0
 
-redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
+redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db, charset='utf-8', decode_responses=True)
 
 prometheus = os.environ.get("PROM_HOST", "http://192.168.1.140:9090/")
 
@@ -16,7 +16,6 @@ prometheus = os.environ.get("PROM_HOST", "http://192.168.1.140:9090/")
 
 while True:
     for job_name in redis_client.keys():
-        job_name = job_name.decode('utf-8')
         query = f'{prometheus}/api/v1/query?query=temperature{{job="{job_name}"}}'
         read_time = value = None
         x = requests.get(query).json()
@@ -29,20 +28,21 @@ while True:
             read_time = datetime.fromtimestamp(tmp[0])
             value = float(tmp[1])
             print(read_time, value)
-        switch_info = redis_client.json().get(job_name)
-        print(switch_info)
-        mod = ""
-        if(switch_info['active']):
-            if (value <= switch_info['setpoint'] - switch_info['delta']):
-                mod = "On"
-            elif (value >= switch_info['setpoint']):
-                mod = "Off"
-        try:
-            switch_query = f"{switch_info['url']}/cm?cmnd=Power%20{mod}"
-            print(switch_query)
-            is_on = requests.get(switch_query).json()['POWER'] == 'ON'
-            redis_client.json().set(job_name, '$.is_on', is_on)
-        except:
-            print("unable to query")
+            switch_info = redis_client.hgetall(job_name)
+            print(switch_info)
+            mod = ""
+            if(int(switch_info['active']) > 0):
+                print("Is Actives")
+                if (value <= float(switch_info['setpoint']) - float(switch_info['delta'])):
+                    mod = "On"
+                elif (value >= float(switch_info['setpoint'])):
+                    mod = "Off"
+            try:
+                switch_query = f"{switch_info['url']}/cm?cmnd=Power%20{mod}"
+                print(switch_query)
+                is_on = requests.get(switch_query).json()['POWER'] == 'ON'
+                redis_client.hset(job_name, 'is_on',  int(is_on))
+            except:
+                print("unable to query")
         
         time.sleep(10)
--- web_app/run.py
+++ web_app/run.py
@@ -13,16 +13,16 @@ redis_client = redis.StrictRedis(host=redis_host, port=redis_port, db=redis_db)
 @app.get('/<string:job>/data')
 def index(job):
     try:
-        status = redis_client.json().get(job)
+        status = redis_client.hgetall(job)
     except:
         abort(404)
-    return f"is_on {1 if status['is_on'] else 0} \nsetpoint {status['setpoint']}"
+    return f"is_on {status['is_on'] } \nsetpoint {status['setpoint']}"
 
 @app.post("/<string:job>/set")
 def setpoint(job):
     sp = request.json
     key = list(sp.keys())[0]
-    redis_client.json().set(job, f"$.{key}", sp[key])
+    redis_client.hset(job, key, sp[key])
     return jsonify(sp)
 
 @app.post("/<string:job>/init")
@@ -30,12 +30,12 @@ def init(job):
     url = request.json['url']
     metadata = {
         "url": url,
-        "active": False,
-        "is_on": False,
+        "active": 0,
+        "is_on": 0,
         "setpoint": 15,
         "delta": 2
     }
-    redis_client.json().set(job, "$", metadata)
+    redis_client.hmset(job, metadata)
     return jsonify(metadata)
 
 if __name__ == '__main__':
